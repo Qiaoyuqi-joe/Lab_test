8f92f0d995c939cf6ef6b0f016b621ed982494d2
Qiaoyuqi-joe <jp2021212806@qmul.ac.uk>
2024-08-16 20:37:45 +0800

    Update the work part

--- my-profile/src/components/Profile-Works.vue
+++ my-profile/src/components/Profile-Works.vue
@@ -1,38 +1,31 @@
 <template>
-    <div class="works">
-      <nav class="sidebar">
-        <ul>
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/education">Education</router-link></li>
-          <li><router-link to="/projects">Projects</router-link></li>
-          <li><router-link to="/honors">Honors</router-link></li>
-          <li><router-link to="/works">Works</router-link></li>
-          <li><router-link to="/skills">Skills</router-link></li>
-          <li><router-link to="/description">Description</router-link></li>
-          <li><router-link to="/news">News</router-link></li>
-        </ul>
-      </nav>
-      <div class="content">
-        <div class="header">
-         
-          <div class="text-content">
-            <h1>Works</h1>
-       <h1>Work Experiences</h1>
-      <h2>●Director of the Student Union Office of the International College Student Union</h2>
-      <p>1. Led the first and second Student Congress of the International College of Beijing University of Posts
-        and Telecommunications.</p>
-      <p>2. Organize and participate in the university fellowship activities to enhance the exchanges and
-        cooperation between colleges and universities.</p>
-      </div>
-        </div>
-        
-      </div>
+  <div class="skills">
+    <LayoutMain>
+      <template v-slot>
+        <div class="text-content">
+          <h1>Works</h1>
+     <h1>Work Experiences</h1>
+    <h2>●Director of the Student Union Office of the International College Student Union</h2>
+    <p>1. Led the first and second Student Congress of the International College of Beijing University of Posts
+      and Telecommunications.</p>
+    <p>2. Organize and participate in the university fellowship activities to enhance the exchanges and
+      cooperation between colleges and universities.</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'UserWorks'
+      </template>
+    </LayoutMain>
+  </div>
+</template>
+
+<script>
+import LayoutMain from './LayoutMain.vue';
+
+export default {
+  name: 'UserWorks',
+  components: { 
+    LayoutMain
   }
-  </script>
-  
+};
+</script>
+
+
+
